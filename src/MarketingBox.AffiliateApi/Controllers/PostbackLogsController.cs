733a3c98007bfae6cb754862f679b8fad35e4f54
Lev Kurbanov <lev.k@smplt.net>
2022-01-26 16:11:14 +0300

    Take affiliate id from token

--- src/MarketingBox.AffiliateApi/Controllers/PostbackLogsController.cs
+++ src/MarketingBox.AffiliateApi/Controllers/PostbackLogsController.cs
@@ -1,9 +1,12 @@
 ﻿using AutoMapper;
 using MarketingBox.AffiliateApi.Authorization;
+using MarketingBox.AffiliateApi.Extensions;
 using MarketingBox.Postback.Service.Grpc;
 using MarketingBox.Postback.Service.Grpc.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,35 +19,47 @@ namespace MarketingBox.AffiliateApi.Controllers
     public class PostbackLogsController : ControllerBase
     {
         private readonly IMapper _mapper;
+        private readonly ILogger<PostbackLogsController> _logger;
         private readonly IEventReferenceLogService _eventReferenceLogService;
 
         public PostbackLogsController(
             IEventReferenceLogService eventReferenceLogService,
-            IMapper mapper)
+            IMapper mapper,
+            ILogger<PostbackLogsController> logger)
         {
             _eventReferenceLogService = eventReferenceLogService;
             _mapper = mapper;
+            _logger = logger;
         }
 
-        [HttpGet("{affiliateId}")]
-        public async Task<ActionResult<List<Models.PostbackLogs.EventReferenceLog>>> GetLogs(
-            [FromRoute]long affiliateId)
+        [HttpGet()]
+        public async Task<ActionResult<List<Models.PostbackLogs.EventReferenceLog>>> GetLogs()
         {
-            var response = await _eventReferenceLogService.GetLogsAsync(
-                new ByAffiliateIdRequest
-                {
-                    AffiliateId = affiliateId
-                });
-
-            if (!response.Success)
+            try
             {
-                return BadRequest(response.ErrorMessage);
-            }
+                var affiliateId = this.GetAffiliateId();
+                var response = await _eventReferenceLogService.GetLogsAsync(
+                    new ByAffiliateIdRequest
+                    {
+                        AffiliateId = affiliateId
+                    });
 
-            return Ok(
-                response.Data is null 
-                ? Enumerable.Empty<Models.PostbackLogs.EventReferenceLog>()
-                : response.Data.Select(_mapper.Map<Models.PostbackLogs.EventReferenceLog>));
+                if (!response.Success)
+                {
+                    ModelState.AddModelError("Error", response.ErrorMessage);
+                    return BadRequest(ModelState);
+                }
+
+                return Ok(
+                    response.Data is null
+                    ? Enumerable.Empty<Models.PostbackLogs.EventReferenceLog>()
+                    : response.Data.Select(_mapper.Map<Models.PostbackLogs.EventReferenceLog>));
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return BadRequest();
+            }
         }
     }
 }
