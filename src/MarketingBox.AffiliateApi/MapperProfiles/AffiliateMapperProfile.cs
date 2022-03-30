affcdd103e235681dbcb4cd1cd30f2ac63c1cb63
Lev Kurbanov <lev.k@smplt.net>
2022-03-30 19:46:12 +0500

    update refs

--- src/MarketingBox.AffiliateApi/MapperProfiles/AffiliateMapperProfile.cs
+++ src/MarketingBox.AffiliateApi/MapperProfiles/AffiliateMapperProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using MarketingBox.Affiliate.Service.Domain.Models.Affiliates;
 using MarketingBox.Affiliate.Service.Grpc.Requests.Affiliates;
 using MarketingBox.AffiliateApi.Models.Affiliates;
 using MarketingBox.AffiliateApi.Models.Affiliates.Requests;
@@ -11,14 +12,14 @@ namespace MarketingBox.AffiliateApi.MapperProfiles
         {
             CreateMap<AffiliateUpsertRequest, AffiliateCreateRequest>();
             CreateMap<AffiliateUpsertRequest, AffiliateUpdateRequest>();
-            CreateMap<AffiliateBank, BankRequest>();
-            CreateMap<AffiliateCompany, CompanyRequest>();
+            CreateMap<AffiliateBank, Bank>();
+            CreateMap<AffiliateCompany, Company>();
             CreateMap<AffiliateGeneralInfo, GeneralInfoRequest>();
 
             CreateMap<Affiliate.Service.Domain.Models.Affiliates.Affiliate, AffiliateModel>()
                 .ForMember(x => x.GeneralInfo, x => x.MapFrom(z => z));
-            CreateMap<Affiliate.Service.Domain.Models.Affiliates.Bank, AffiliateBank>();
-            CreateMap<Affiliate.Service.Domain.Models.Affiliates.Company, AffiliateCompany>();
+            CreateMap<Bank, AffiliateBank>();
+            CreateMap<Company, AffiliateCompany>();
             CreateMap<Affiliate.Service.Domain.Models.Affiliates.Affiliate, AffiliateGeneralInfo>();
         }
     }
