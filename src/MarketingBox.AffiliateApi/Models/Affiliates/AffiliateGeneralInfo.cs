b8219401f5b4a35088fba90de7aa58e9c9987daa
Lev Kurbanov <lev.k@smplt.net>
2022-04-01 22:17:20 +0500

    refactoring, update refs

--- src/MarketingBox.AffiliateApi/Models/Affiliates/AffiliateGeneralInfo.cs
+++ src/MarketingBox.AffiliateApi/Models/Affiliates/AffiliateGeneralInfo.cs
@@ -1,25 +1,22 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using MarketingBox.Affiliate.Service.Domain.Models.Affiliates;
-using MarketingBox.Affiliate.Service.Domain.Models.Brands;
 using MarketingBox.Affiliate.Service.Domain.Models.Common;
 
 namespace MarketingBox.AffiliateApi.Models.Affiliates
 {
     public class AffiliateGeneralInfo
     {
-        [Required]
-        public string Username { get; set; }
-        [Required]
-        public string Password { get; set; }
-        [Required]
-        public string Email { get; set; }
-        [Required]
-        public string Phone { get; set; }
+        [Required] public string Username { get; set; }
+        [Required] public string Password { get; set; }
+        [Required] public string Email { get; set; }
+        [Required] public string Phone { get; set; }
         public string Skype { get; set; }
         public string ZipCode { get; set; }
+
         [DefaultValue(MarketingBox.Affiliate.Service.Domain.Models.Affiliates.State.Active)]
         public State? State { get; set; }
+
         [DefaultValue(MarketingBox.Affiliate.Service.Domain.Models.Common.Currency.USD)]
         public Currency? Currency { get; set; }
     }
