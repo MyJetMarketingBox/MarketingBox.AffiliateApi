6ef71fc63f0427f6ff123d23427c1ad27fdaf07c
Alexey <alexey.p@smplt.net>
2022-04-21 15:28:56 +0300

    update cors settings

--- src/MarketingBox.AffiliateApi/Startup.cs
+++ src/MarketingBox.AffiliateApi/Startup.cs
@@ -44,7 +44,7 @@ namespace MarketingBox.AffiliateApi
                     {
                         builder
                             .AllowAnyOrigin()
-                            .AllowCredentials()
+                            //.AllowCredentials()
                             .AllowAnyHeader()
                             .AllowAnyMethod();
                     });
