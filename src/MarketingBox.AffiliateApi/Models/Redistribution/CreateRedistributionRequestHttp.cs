da6a3fda8c7b4e8bb913e26b6795aff57d78612e
Alexey <alexey.p@smplt.net>
2022-05-04 14:16:37 +0300

    refactoring

--- src/MarketingBox.AffiliateApi/Models/Redistribution/CreateRedistributionRequestHttp.cs
+++ src/MarketingBox.AffiliateApi/Models/Redistribution/CreateRedistributionRequestHttp.cs
@@ -17,6 +17,7 @@ namespace MarketingBox.AffiliateApi.Models.Redistribution
         public int PortionLimit { get; set; }
         [Required]
         public int DayLimit { get; set; }
+        [Required]
         public bool UseAutologin { get; set; }
         public List<long> RegistrationsIds { get; set; }
         public List<long> FilesIds { get; set; }
