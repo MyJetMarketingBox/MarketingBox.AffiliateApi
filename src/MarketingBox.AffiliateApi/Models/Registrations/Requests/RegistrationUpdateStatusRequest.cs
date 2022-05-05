f875d20b7644bfec6ff00db536794b5974abaa6e
Lev Kurbanov <lev.k@smplt.net>
2022-05-05 15:26:37 +0500

    fix update status request model

--- src/MarketingBox.AffiliateApi/Models/Registrations/Requests/RegistrationUpdateStatusRequest.cs
+++ src/MarketingBox.AffiliateApi/Models/Registrations/Requests/RegistrationUpdateStatusRequest.cs
@@ -5,7 +5,7 @@ namespace MarketingBox.AffiliateApi.Models.Registrations.Requests
 {
     public class RegistrationUpdateStatusRequest
     {
-        [Required] public RegistrationStatus Status { get; set; }
+        [Required] public RegistrationStatus? Status { get; set; }
         [Required] public string Comment { get; set; }
     }
 }
