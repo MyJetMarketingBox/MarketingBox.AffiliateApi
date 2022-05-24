ce02d09761c94c408fc857379229b832bdcf0200
Lev Kurbanov <lev.k@smplt.net>
2022-05-24 21:49:02 +0500

    update refs

--- src/MarketingBox.AffiliateApi/Models/Campaigns/CampaignModel.cs
+++ src/MarketingBox.AffiliateApi/Models/Campaigns/CampaignModel.cs
@@ -1,9 +1,13 @@
-﻿namespace MarketingBox.AffiliateApi.Models.Campaigns
+﻿using System;
+
+namespace MarketingBox.AffiliateApi.Models.Campaigns
 {
     public class CampaignModel
     {
         public long Id { get; set; }
 
         public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastActiveAt { get; set; }
     }
 }
