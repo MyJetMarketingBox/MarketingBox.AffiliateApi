436935d995307b488c5dfde6b3761a193125dae2
Lev Kurbanov <lev.k@smplt.net>
2022-05-27 16:34:21 +0500

    fix response

--- src/MarketingBox.AffiliateApi/Models/Registrations/RegistrationsFileHttp.cs
+++ src/MarketingBox.AffiliateApi/Models/Registrations/RegistrationsFileHttp.cs
@@ -5,6 +5,7 @@ namespace MarketingBox.AffiliateApi.Models.Registrations
     public class RegistrationsFileHttp
     {
         public long Id { get; set; }
+        public string FileName { get; set; }
         public long CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
     }
